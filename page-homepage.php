450c3ccc2227d8a9f5385512e8f32912ebf6bbf2
nanda <nandapuspitarana@gmail.com>
2018-05-17 10:22:56 +0700

    responsive

--- page-homepage.php
+++ page-homepage.php
@@ -23,7 +23,7 @@ get_header('homepage');
 			<div class="row">
 				<div class="col">
 					<nav class="navbar navbar-expand-lg navbar-light bg-light wt-navbar">
-						<a href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-header.png" width="" height="" alt="" class="wt-logo-header"></a>
+						<a href="/" class="wt-link-logo-header"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-header.png" width="" height="" alt="" class="wt-logo-header"></a>
 						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="navbar-toggler-icon"></span>
 						</button>
